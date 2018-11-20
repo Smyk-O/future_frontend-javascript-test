46c81370ae293872980bf4175f15b982a1a5f2c5
Oleg Smyk <Smyk006@yandex.ru>
2018-11-20 23:30:25 +0300

    пагинация + вывод полной информации о персоне

--- src/App.js
+++ src/App.js
@@ -1,45 +1,79 @@
 import React, { Component } from 'react';
-import { Button, Container } from 'reactstrap';
+import { Button, Container, Pagination, PaginationItem, PaginationLink } from 'reactstrap';
 import './App.css';
+
+const list_little_url = 'http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}';
+const list_large_url = 'http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: []
+      items: [],
+      list_page_active: false,
+      number_of_pages: false,
+      item_target: false
+
     };
   }
 
-
   loadingList = async (listSize) => {
 
-    if (listSize === "litle") {
+    if (listSize === "little") {
       const list_url = await
-        fetch('http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}');
+        fetch(list_little_url);
       const list_data = await list_url.json();
-
+      const list_page_size = Math.ceil(list_data.length / 30);
       this.setState({
-        items: list_data
+        items: list_data,
+        list_page_active: 1,
+        number_of_pages: list_page_size
       });
     }
 
     else {
       const list_url = await
-        fetch('http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}');
+        fetch(list_large_url);
       const list_data = await list_url.json();
-
+      const list_page_size = Math.ceil(list_data.length / 30);
       this.setState({
-        items: list_data
+        items: list_data,
+        list_page_active: 1,
+        number_of_pages: list_page_size
       });
     }
 
   }
 
+  targetItem() {
+
+    if (this.state.item_target) {
+      const item = this.state.item_target;
+      return (
+        <div className="fullInfo">
+        <div>Выбран пользователь <b>{item.firstName + " " + item.lastName}</b></div>
+        <div>Описание:</div>
+        <textarea placeholder={item.description}></textarea>
+        <div>Адрес проживания: <b>item.address.streetAddress</b></div>
+        <div>Город: <b>item.address.city</b></div>
+        <div>Провинция/штат: <b>item.address.state</b></div>
+        <div>Индекс: <b>item.address.zip</b></div>
+        </div>
+      )
+    }
+    return null
+  }
+
   getItem() {
+
     if (this.state.items.length > 0) {
-      return this.state.items.map((item) => {
+      const firstElem = (this.state.list_page_active - 1) * 30;
+      const lastElem = firstElem + 29;
+      const selectionItems = this.state.items.slice(firstElem, lastElem);
+
+      return selectionItems.map((item) => {
         return (
-          <div className="item_table" key={item.id}>
+          <div className="item_table" key={item.id + item.firstName} onClick={(() => this.setState({ item_target: item }))}>
             <div className="table_id">{item.id}</div>
             <div className="table_firstName">{item.firstName}</div>
             <div className="table_lastName">{item.lastName}</div>
@@ -48,19 +82,48 @@ class App extends Component {
           </div >
         )
       });
+
     } if (!this.state.items) {
       return (<div className="loading">loading...</div>)
-    } else {return(<div className="choose">choose a list</div>)}
+    } else { return (<div className="choose">choose a list</div>) }
   }
 
+  getPage() {
+    let page_btn = []
+    for (let i = 0; i < this.state.number_of_pages; i++) {
+      page_btn.push(
+        <PaginationItem key={i + "page"} onClick={(() => this.setState({ list_page_active: i + 1 }))}>
+          <PaginationLink>
+            {i + 1}
+          </PaginationLink>
+        </PaginationItem>
+      )
+    }
+
+    if (this.state.number_of_pages > 1) {
+      return (
+        <Pagination aria-label="Page navigation example" >
+          <PaginationItem onClick={(() => this.setState({ list_page_active: this.state.list_page_active - 1 }))}>
+            <PaginationLink previous />
+          </PaginationItem>
+          {page_btn}
+          <PaginationItem onClick={(() => this.setState({ list_page_active: this.state.list_page_active + 1 }))}>
+            <PaginationLink next />
+          </PaginationItem>
+        </Pagination>
+      )
+
+    }
+
+    return null
+  }
 
   render() {
-    console.log(this.state.item)
     return (
       <Container className="app">
         <div className="button-group">
-          <Button outline color="secondary" onClick={(() => {this.loadingList("litle"); this.setState({items: false })})}>little list</Button>
-          <Button outline color="secondary" onClick={(() => {this.loadingList("large"); this.setState({items: false })})}>large list</Button>
+          <Button outline color="secondary" onClick={(() => { this.loadingList("little"); this.setState({ items: false }) })}>little list</Button>
+          <Button outline color="secondary" onClick={(() => { this.loadingList("large"); this.setState({ items: false }) })}>large list</Button>
         </div>
 
         <div>
@@ -72,8 +135,10 @@ class App extends Component {
             <div className="table_phone">phone</div>
           </div >
           {this.getItem()}
-        </div>
 
+        </div>
+        {this.getPage()}
+        {this.targetItem()}
       </Container>
     );
   }
